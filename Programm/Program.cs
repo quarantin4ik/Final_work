73f2b5e98c926e4daa8621160095517ab5f05cea
Pavel <pavel@example.com>
2024-02-06 22:35:35 +0300

    add code

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -7,3 +7,30 @@
 //[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array_1 = {"123", "1567", "-2", "computer science", "Hello", "2", "world", ":-)"};
+string[] array_2 = new string[array_1.Length];
+
+void three_symbol(string[] array, string[] new_array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+            {
+                new_array[count] = array[i];
+                count++;
+            }
+    }
+}
+
+void print_array(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+three_symbol(array_1, array_2);
+print_array(array_2);
